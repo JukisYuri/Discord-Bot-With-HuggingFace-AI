1aed56cfedfa24d7c68623d90bc68ab612c3669c
JukisYuri <23130092@st.hcmuaf.edu.vn>
2024-12-03 12:28:43 +0700

    Some stuffs fixed

--- autismbot.js
+++ autismbot.js
@@ -18,7 +18,7 @@ client.on('ready', () => {
 const IGNORE_PREFIX = "!";
 
 // Kênh ID được cấp quyền cho bot hoạt động
-const CHANNELS = [`1177200241905242152`, `1084866665034031106`, `1177195977136939028`];
+const CHANNELS = [`1177200241905242152`, `1084866665034031106`, `1177195977136939028`, `1313376059030507590`];
 
 // ID người sẽ dùng đến bot
 const OWNER_ID = '607183227911667746';
@@ -44,11 +44,11 @@ client.on('messageCreate', async (message) => {
     let botActive = false;
     try {
         // Kiểm tra nếu nhận lệnh bật bot (giả)
-        if (message.content.includes("Hầu gái của tôi đâu")) {
+        if (message.content.includes("Hầu gái đâu")) {
             botActive = true; // Bật bot
             await message.channel.sendTyping(); // Hiệu ứng typing
             setTimeout(async () => {
-                await message.reply("Hân hạnh được phục vụ chủ nhân!");
+                await message.reply("Chủ nhân cho gọi em~");
             }, 2000); // Thời gian typing giả lập
             return;
         }
